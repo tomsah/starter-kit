1434582959e024fe13cdf6a0ef48326553dfc603
Thomas Salah <thomas.salah@gmail.com>
2020-05-11 19:16:29 +0100

    Refactor: creating a reusable utils to test custom hooks

--- base/src/shared/rtl-example/__tests__/custom-hook.js
+++ base/src/shared/rtl-example/__tests__/custom-hook.js
@@ -3,19 +3,38 @@ import React from 'react'
 import {render, act} from '@testing-library/react'
 import {useCounter} from '../use-counter'
 
-test('expose the count and increment/decrement', () => {
-  let result
+function setup({initialPops} = {}) {
+  const result = {}
   // a custom hooks cannot be call by itself so test it you have
   // to wrap it in a fake component
-  function TestComponent() {
-    result = useCounter()
+  function TestComponent(props) {
+    result.current = useCounter(props)
     return null
   }
 
-  render(<TestComponent />)
-  expect(result.count).toBe(0)
-  act(() => result.increment())
-  expect(result.count).toBe(1)
-  act(() => result.decrement())
-  expect(result.count).toBe(0)
+  render(<TestComponent {...initialPops} />)
+  return result
+}
+
+test('expose the count and increment/decrement', () => {
+  const result = setup()
+  expect(result.current.count).toBe(0)
+  act(() => result.current.increment())
+  expect(result.current.count).toBe(1)
+  act(() => result.current.decrement())
+  expect(result.current.count).toBe(0)
+})
+
+test('allows customization of the initial count', () => {
+  const result = setup({initialPops: {initialCount: 3}})
+  expect(result.current.count).toBe(3)
+})
+
+test('allows customization of the step', () => {
+  const result = setup({initialPops: {step: 2}})
+  expect(result.current.count).toBe(0)
+  act(() => result.current.increment())
+  expect(result.current.count).toBe(2)
+  act(() => result.current.decrement())
+  expect(result.current.count).toBe(0)
 })
